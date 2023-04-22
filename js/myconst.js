47021007ed668ff90afe6234aa6d6e75944b38bf
marat-landed <mrtland7@gmail.com>
2023-04-22 23:51:08 +0300

    v 37

--- js/myconst.js
+++ js/myconst.js
@@ -1,4 +1,3 @@
-/*
 const param_scale = [ 
     1, // temp, min 
 	1, // temp, max
@@ -22,36 +21,11 @@ const openweathermap_place =
 				[ 9.005401, 38.763611, "Addis_Ababa", "Аддис-Абеба, Эфиопия" ],
 				[ 63.460899, 142.785812, "Oymyakon", "Оймякон, Россия" ],
 				[ -77.846, 166.676, "McMurdo", "Станция Мак-Мердо, США" ] ];
-//const database_URL = "https://open-weather-map-2-default-rtdb.europe-west1.firebasedatabase.app";
-//export const database_URL = "https://probe-web-default-rtdb.europe-west1.firebasedatabase.app"; // Probe-web
-//const Chart_title_arr = ['Температура минимальная', 'Температура максимальная', 'Давление', 'Влажность', 'Скорость ветра', 'Направление ветра', 'Облачность', 'Вероятность осадков', 'Дождь', 'Снег'];
-//const yAxis_title_arr = ['градусов', 'градусов', 'гПа', '%', 'м/с', 'градусов', '%', '%', 'мм', 'мм'];
+const database_URL = "https://open-weather-map-2-default-rtdb.europe-west1.firebasedatabase.app";
+//const database_URL = "https://probe-web-default-rtdb.europe-west1.firebasedatabase.app"; // Probe-web
+const Chart_title_arr = ['Температура минимальная', 'Температура максимальная', 'Давление', 'Влажность', 'Скорость ветра', 'Направление ветра', 'Облачность', 'Вероятность осадков', 'Дождь', 'Снег'];
+const yAxis_title_arr = ['градусов', 'градусов', 'гПа', '%', 'м/с', 'градусов', '%', '%', 'мм', 'мм'];
 
-//export {param_scale, param_name_str, openweathermap_place, database_URL};
-*/
+export {param_scale, param_name_str, openweathermap_place, database_URL};
 
-module.exports = {
-  database_URL: 'https://open-weather-map-2-default-rtdb.europe-west1.firebasedatabase.app',
-  Chart_title_arr: ['Температура минимальная', 'Температура максимальная', 'Давление', 'Влажность', 'Скорость ветра', 'Направление ветра', 'Облачность', 'Вероятность осадков', 'Дождь', 'Снег'],
-  yAxis_title_arr: ['градусов', 'градусов', 'гПа', '%', 'м/с', 'градусов', '%', '%', 'мм', 'мм'],
-  openweathermap_place: [ [ 49.9541962, 36.0966085, "Pisochyn", "Песочин, Украина, Харьковская область" ],
-				[ 61.217381, -149.863129, "Anchorage", "Анкоридж, США, Аляска" ],
-				[ -54.81084, -68.31591, "Ushuaia", "Ушуая, Аргентина, Огненная Земля" ],
-				[ 9.005401, 38.763611, "Addis_Ababa", "Аддис-Абеба, Эфиопия" ],
-				[ 63.460899, 142.785812, "Oymyakon", "Оймякон, Россия" ],
-				[ -77.846, 166.676, "McMurdo", "Станция Мак-Мердо, США" ] ],
-  param_name_str: [ [ "temp", "min" ], [ "temp", "max" ], [ "pressure" ], [ "humidity" ], 
-			  [ "wind_speed" ], [ "wind_deg" ], [ "clouds" ], [ "pop" ], [ "rain" ], 
-			  [ "snow" ], [ "weather", "icon" ] ],
-  param_scale: [ 
-    1, // temp, min 
-	1, // temp, max
-	2, // pressure		Изменить на 2
-	5, // humidity
-	0.5, // wind_speed
-	10, // wind_deg 		Изменить на 10
-	10, // clouds		Изменить на 10
-	10, // pop			Изменить на 10
-	2, // rain			Изменить на 2
-	2 ]
-};
+
