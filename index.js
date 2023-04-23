e9ba3daed7544acb01efadc274416c9c77273562
marat-landed <mrtland7@gmail.com>
2023-04-23 19:47:11 +0300

    v 71

--- index.js
+++ index.js
@@ -142,6 +142,7 @@ import { plot_all_forecasts } from "./js/plot_all_forecasts.js";
 	elements[i].onclick = function(evt){
       var tabcontent, tablinks;
 	  tabcontent = document.getElementsByClassName("tabcontent");
+	  console.log(tabcontent);
 	  for (let i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	  }
