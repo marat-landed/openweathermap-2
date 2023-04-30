0ec416d34ff8233417978f70f2f6049f6b701fb4
marat-landed <mrtland7@gmail.com>
2023-04-30 11:47:38 +0300

    v 214

--- js/plot_err_grath.js
+++ js/plot_err_grath.js
@@ -70,10 +70,11 @@ function plot_err_grath(all_dist, Chart_title_arr, yAxis_title_arr, param_scale)
 		  sum_el += myArray[k];
        	  sum_err	+= myArray[k]*k*param_scale[p_name_num];
 	    }
+		let mean_error;
 		if (sum_el != 0)
-	      let mean_error = (sum_err/sum_el).toFixed(1); // округляем до первого знака
+	      mean_error = (sum_err/sum_el).toFixed(1); // округляем до первого знака
 	    else
-		  let mean_error = 0;
+		  mean_error = 0;
 		console.log(sum_err,sum_el,mean_error);
 	    data_err.push(mean_error);
 	  }
