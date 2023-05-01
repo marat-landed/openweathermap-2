0683bc29a0e506e18d09d03966b370bed4bb2611
marat-landed <mrtland7@gmail.com>
2023-05-01 20:28:39 +0300

    Update plot_err_grath.js

--- js/plot_err_grath.js
+++ js/plot_err_grath.js
@@ -96,9 +96,9 @@ function create_chart_error_mean(renderTo, Chart_title, yAxis_title) {
       }
 	},
 	legend: {
-        width: 400,
+        width: 100%, //400,
         floating: true,
-        align: 'left',
+        align: 'bottom', //'left',
         //x: 70, // = marginLeft - default spacingLeft
         //itemWidth: 100,
         borderWidth: 1,
